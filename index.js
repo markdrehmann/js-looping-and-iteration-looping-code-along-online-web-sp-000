c2623a3299fef440c36f58dfb00e1b2fa0adc9b0
markdrehmann <markdrehmann@gmail.com>
2021-02-19 22:42:55 -0600

    commit tree 873268270dc6e7690b68af7c15e4a4f57d4abdd0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let cardArray = []
+  for (let i = 0; i < names.length; i++) {
+    cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return cardArray
+}
+
+function countDown(number) {
+  while ( number > 0 ) {
+    console.log(number);
+    number -= 1;
+  }
+  console.log(number)
+}
